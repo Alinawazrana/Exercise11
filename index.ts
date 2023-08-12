8be01dc6481d7d95fcd24e00241556fb4d6028d5
Alinawaz <139918066+TypescriptAlinawaz@users.noreply.github.com>
2023-08-12 21:16:02 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let Names : string [] = ['Ali ',  'Ahmed' ,  'Saad' , 'Raza' ,  'aabid'] ;
+
+console.log(Names[0])
+
+console.log(Names[1])
+
+console.log(Names[2])
+
+console.log(Names[3])
+
+console.log(Names[4])
+
+
+
